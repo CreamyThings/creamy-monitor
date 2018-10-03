31abbee0e0727a4dd6c4cb56182d6c4d7a89a127
AlbinoDrought <albinodrought@gmail.com>
2018-10-02 19:31:21 -0700

    feat: add barebones in-memory store and `setInterval` """job scheduler"""

---
+++ src/monitors/repositoryDebug.js
@@ -0,0 +1,13 @@
+module.exports = (emitter) => {
+  emitter.on('create', (monitor) => {
+    console.log('created', monitor);
+  });
+
+  emitter.on('update', (monitor) => {
+    console.log('updated', monitor);
+  })
+
+  emitter.on('delete', (monitor) => {
+    console.log('deleted', monitor);
+  });
+};
---
+++ src/runner.js
@@ -0,0 +1,17 @@
+const repository = require('./monitors/repository');
+const bootstrapToService = require('./monitors/to-service/bootstrap');
+const repositoryDebug = require('./monitors/repositoryDebug');
+
+bootstrapToService(repository.emitter);
+repositoryDebug(repository.emitter);
+
+// insert test monitor
+repository.upsertGraph({
+  name: 'test monitor',
+  type: 'to-service',
+  interval: 30,
+  request: {
+    url: 'http://albinodrought.com',
+    method: 'get',
+  }
+});
