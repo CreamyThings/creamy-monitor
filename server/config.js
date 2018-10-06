2028dc8086f0befeae09d93f56bf2e8c9556f288
Khoa Nguyen <khoa.tan.nguyen.96@gmail.com>
2018-10-05 19:36:13 -0700

    fix(backend): load dotenv as early as possible

--- server/config.js
+++ server/config.js
@@ -2,6 +2,7 @@
 
 module.exports = {
   PORT: process.env.PORT || 8080,
+  API_ORIGIN: process.env.API_ORIGIN || 'http://localhost:8080',
   CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
   JWT_SECRET: process.env.JWT_SECRET,
   JWT_EXPIRY: process.env.JWT_EXPIRY || '7d'
