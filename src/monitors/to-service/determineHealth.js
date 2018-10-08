f98d11730f09ce274f61eb10b81320ad900e36cb
AlbinoDrought <albinodrought@gmail.com>
2018-10-08 13:31:23 -0700

    refactor(backend): make a nice rule map
    maps rules into array of { passing, text }

--- src/monitors/to-service/determineHealth.js
+++ src/monitors/to-service/determineHealth.js
@@ -5,15 +5,28 @@ const assertNoError = {
   text: data => `error ${data.error === null ? 'is' : 'is not'} null`,
 };
 
+const makeVerboseRules = (rules, data) => rules.map(rule => ({
+  passed: rule.test(data),
+  text: rule.text(data),
+}));
+
 module.exports = (monitor, data) => {
   const rules = [
     assertNoError,
     ...ruleParser.mapRules(monitor.rules || []),
   ];
 
-  const healthy = rules.every(rule => rule.test(data));
+  const actions = makeVerboseRules(rules, data);
+  const healthy = actions.every(action => action.passed);
+
+  const event = {
+    type: 'to-service',
+    healthy,
+    time: (new Date()).getTime(),
+    actions,
+  };
 
   console.log(monitor.id, 'healthy?', healthy ? 'yes' : 'no');
 
-  return healthy;
+  return event;
 };
