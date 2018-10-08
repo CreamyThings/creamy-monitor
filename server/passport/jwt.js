d8db18af4023827f6d366cba9d0c697961760b52
AlbinoDrought <albinodrought@gmail.com>
2018-10-08 11:35:17 -0700

    fix(backend): use configured JWT secret instead of Pancake

--- server/passport/jwt.js
+++ server/passport/jwt.js
@@ -5,7 +5,7 @@ const { Strategy: JwtStrategy, ExtractJwt } = require('passport-jwt');
 const { JWT_SECRET } = require('../config');
 
 const options = {
-  secretOrKey: 'Pancake',
+  secretOrKey: JWT_SECRET,
   jwtFromRequest: ExtractJwt.fromAuthHeaderWithScheme('Bearer'),
   algorithms: ['HS256']
 };
